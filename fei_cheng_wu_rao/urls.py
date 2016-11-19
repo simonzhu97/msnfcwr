059c2b9da2dd61b0c43bd962e3cf9fd6cb21eec9
zeizyy <yz9fy@virginia.edu>
2016-11-19 14:14:42 -0500

    url

--- fei_cheng_wu_rao/urls.py
+++ fei_cheng_wu_rao/urls.py
@@ -6,6 +6,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'fei_cheng_wu_rao.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^angel/', include(admin.site.urls)),
     url(r'^', include('liu_yan_ban.urls')),
 )
